50ef37fb34526afeb050d3e7b5a4defac07edb87
João Pedro Lourenço dos Santos <joao.santos1776@etec.sp.gov.br>
2024-06-06 00:28:27 -0300

    tela de login iniciada

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,22 @@
 import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
     <div className="Container login">
       <div className="title">
         <h1>Login</h1>
+      </div>
+
+      <div className="login-form">
+        <form>
+          <h1>Inicie sua sessão!</h1>
+
+          <input type="email" id="email" name="email" placeholder="Email" />
+          <input type="password" id="senha" name="senha" placeholder="Senha" />
+
+          <button>Confirmar</button>
+        </form>
       </div>
     </div>
   );
